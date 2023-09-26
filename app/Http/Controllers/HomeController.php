3af9cf365b695d53c1d43d97ba5eb3c94e6992b9
Alya Raisa Hidayat <alyaraisahidayat@Alyas-MacBook-Pro.local>
2023-09-26 13:48:10 +0700

    week 5

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('home.userpage');
+    }
+
+    public function redirect()
+    {
+    // Check if a user is authenticated
+    if (Auth::check()) {
+        $usertype = Auth::user()->usertype;
+
+        if ($usertype == "1") {
+            return view('admin.home');
+        } else {
+            return view('home.userpage');
+        }
+    } else {
+        // User is not authenticated, handle accordingly
+        return redirect()->route('login'); // Redirect to the login page or another appropriate route
+    }
+}
+
+}
---
+++ resources/views/home/product.blade.php
@@ -0,0 +1,316 @@
+<section class="product_section layout_padding">
+         <div class="container">
+            <div class="heading_container heading_center">
+               <h2>
+                  Our <span>products</span>
+               </h2>
+            </div>
+            <div class="row">
+               <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           Tambah
+                           </a>
+                           <a href="" class="option2">
+                           Beli Sekarang
+                           </a>
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p1.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Tepung Segitiga <br>Biru 100gr 
+                        </h5>
+                        <h6>
+                           Rp10.000
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           Tambah
+                           </a>
+                           <a href="" class="option2">
+                           Beli Sekarang
+                           </a>
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p2.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Royco Ayam Saset <br><br>
+                        </h5>
+                        <h6>
+                           Rp1.000
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           Tambah
+                           </a>
+                           <a href="" class="option2">
+                           Beli Sekarang
+                           </a>
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p3.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Blue Band Serbaguna <br><br>
+                        </h5>
+                        <h6>
+                           Rp7.000
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           Tambah
+                           </a>
+                           <a href="" class="option2">
+                           Beli Sekarang
+                           </a>
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p4.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Loyang Oven <br><br>
+                        </h5>
+                        <h6>
+                           Rp35.000
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           Tambah
+                           </a>
+                           <a href="" class="option2">
+                           Beli Sekarang
+                           </a>
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p5.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Susu Kental Manis <br>Frisian Flag
+                        </h5>
+                        <h6>
+                           $75
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           Tambah
+                           </a>
+                           <a href="" class="option2">
+                           Beli Sekarang
+                           </a>
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p6.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Gulaku 100gr <br><br>
+                        </h5>
+                        <h6>
+                           Rp12.000
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           Tambah
+                           </a>
+                           <a href="" class="option2">
+                           Beli Sekarang
+                           </a>
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p7.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Bubblewarp
+                        </h5>
+                        <h6>
+                           Rp.20.000
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           Tambah
+                           </a>
+                           <a href="" class="option2">
+                           Beli Sekarang
+                           </a>
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p8.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Susu Full Cream <br>Greenfields
+                        </h5>
+                        <h6>
+                           Rp18.000
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           Tambah
+                           </a>
+                           <a href="" class="option2">
+                           Beli Sekarang
+                           </a>
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p9.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Bon Cabe Level 10 <br><br>
+                        </h5>
+                        <h6>
+                           Rp10.000
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           Tambah
+                           </a>
+                           <a href="" class="option2">
+                           Beli Sekarang
+                           </a>
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p10.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Ladaku <br><br>
+                        </h5>
+                        <h6>
+                           Rp1.000
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           Tambah
+                           </a>
+                           <a href="" class="option2">
+                           Beli Sekarang
+                           </a>
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p11.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Baking Powder Koepoe <br><br>
+                        </h5>
+                        <h6>
+                           Rp8.000
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-sm-6 col-md-4 col-lg-4">
+                  <div class="box">
+                     <div class="option_container">
+                        <div class="options">
+                           <a href="" class="option1">
+                           Tambah
+                           </a>
+                           <a href="" class="option2">
+                           Beli Sekarang
+                           </a>
+                        </div>
+                     </div>
+                     <div class="img-box">
+                        <img src="images/p12.png" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           Telur 100gr <br><br>
+                        </h5>
+                        <h6>
+                           Rp21.000
+                        </h6>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="btn-box">
+               <a href="">
+               View All products
+               </a>
+            </div>
+         </div>
+</section>
